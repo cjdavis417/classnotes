fb147ff4cbbc8902f9ed43487dcd2ef7bb9834ab
Christopher Davis <christopherjdavis417@gmail.com>
2018-01-31 21:49:37 -0500

    added array page

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    // this or statement accomodates for safari OR chrome, firefox, ie, and opera.
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("button-to-top").style.display = "block";
+    } else {
+        document.getElementById("button-to-top").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;  // for safari
+    document.documentElement.scrollTop = 0; // for chrome, firefox, ie, and opera
+}
