a1e07f1c587702055694a2c22f07477c195a3f74
Christopher Davis <christopherjdavis417@gmail.com>
2018-03-25 12:31:12 -0400

    added ajax page

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,26 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // for chrome, firefox, ie, and opera
 }
 
+
+// ****** beginning of masonry.pkgd.js stuff ****** //
+
+// adds masonry attribute and value to all articles
+var article = document.getElementsByTagName('article');
+
+for (var i = 0; i < article.length; i++) {
+    article[i].classList.add('grid-item')
+}
+
+// initialize masonry
+var grid = document.querySelector('.grid');
+var msnry = new Masonry( grid, {
+    itemSelector: '.grid-item',
+    //columnWidth: 200,
+    gutter: 5,
+    horizontalOrder: true,
+    fitWidth: true,
+    // originLeft: true,
+    // origintTop: true
+});
+
+
--- scripts/nav.js
+++ scripts/nav.js
@@ -91,6 +91,15 @@ var navHeaders = [
                 href: 'codeconverter.html'
             }
         ]
+    },
+    ajax = {
+        name: 'AJax',
+        subheader: [
+            basics = {
+                name: 'Basics',
+                href: 'ajaxbasics.html'
+            }
+        ]
     }
 ]
 
@@ -148,33 +157,3 @@ for (var i = 0; i < navHeaders.length; i++){
     nav.appendChild(navUL);
 };
 
-
-
-
-
-
-// nav.innerHTML = '<!- navigation is generated content from scripts/nav.js ->\
-// <ul>\
-//     <li><a href="index.html">Home</a></li>\
-//     <li><div class="dropdown">HTML\
-//         <div class="dropdown-content">\
-//             <a href="forms.html">Forms</a>\
-//         </div></div></li>\
-//     <li><div class="dropdown">CSS\
-//             <div class="dropdown-content">\
-//                 <a href="selectors.html">Selectors</a>\
-//                 <a href="pseudoElements.html">Pseudo Elements</a>\
-//             </div></div></li>\
-//     <li><a href="consolenotes.html">Console</a></li>\
-//     <li><a href="gitbasics.html">GIT Basics</a></li>\
-//     <li><a href="github.html">GIT Hub</a></li>\
-//     <li><a href="ruby.html">Ruby</a></li>\
-//     <li><div class="dropdown">JavaScript\
-//         <div class="dropdown-content">\
-//             <a href="loops.html">Loops</a>\
-//             <a href="arrays.html">Arrays</a>\
-//             <a href="objects.html">Objects</a>\
-//             <a href="helpful.html">Helpful Snippets</a>\
-//             <a href="codeconverter.html">Code Converter</a>\
-//         </div></div></li>\
-// </ul>';
